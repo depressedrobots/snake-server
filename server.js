d518567cf2ffbc02bc073d440992a9063abce5d3
WAJurczyk <wojtschek@web.de>
2014-03-25 23:18:20 +0100

    added web-client

--- server.js
+++ server.js
@@ -214,3 +214,25 @@ var getUpdateObject = function() {
 
 	return update;
 }
+
+/*
+ * the HTML WEB SERVER PART
+ */
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
